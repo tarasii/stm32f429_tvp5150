44945d6eb14a04f3cb1e58022b74e84ee9b4f731
tarasii <tarasii@gmail.com>
2015-09-17 23:34:41 +0300

    more functions

--- Inc/svga050.h
+++ Inc/svga050.h
@@ -44,7 +44,7 @@ typedef struct
 	bool VericalPolarity;    //
 	bool HorisontalPolarity; //
 	uint8_t VAL;
-}SVGA_SP_StructTypeDef; // V sync/H sync Polarity& 3D function
+}SVGA_SP_StructTypeDef; // V sync/H sync Polarity& 3D structure
 
 typedef enum
 {
@@ -61,6 +61,46 @@ typedef enum
   SVGA_SLP_Before = 0x02  /*!< 1 line before Even field */
 }SVGA_SLP_TypeDef; // V Offset : Adjust odd field active video start position when interlaced video mode
 
+typedef enum
+{
+  SVGA_SD_1_1   = 0x00, /*!< 1:1   */
+  SVGA_SD_11_10 = 0x01, /*!< 11:10 */
+  SVGA_SD_9_8   = 0x02  /*!< 9:8   */
+}SVGA_SD_TypeDef; // H Scale : Horizontal 4/3downscale for PAL/NTSC
+
+typedef struct
+{
+	bool V_Scale;            //0 - 1:1; 1 - 6:5
+	SVGA_SD_TypeDef H_Scale; //0 - 1:1; 1 - 11:10; 2 - 9:8
+	uint8_t VAL;
+}SVGA_DS_StructTypeDef; // Down scaling for NTSC & PAL video structure
+
+typedef struct
+{
+	bool PDOWN;   // All system power off
+	bool BSGENPD; // Discharge current generator power off
+	bool RDACPD;  // DAC module power off
+	bool RAMPPD;  // DAC Buffer module power off
+	bool VCOMPD;  // Vcom power off
+	bool TSENPD;  // Temperature sensor power off
+	uint8_t VAL;
+}SVGA_PD_StructTypeDef; // Power down structure
+
+typedef struct
+{
+	bool DispOff; // 0 - ON; 1 - OFF
+	bool VSCAN;   // 0 - Top > Bottom; 1 - Bottom > Top
+	bool HSCAN;   // 0 - Left > Right; 1 - Right > Left
+	uint8_t VAL;
+}SVGA_DO_StructTypeDef; // Power down structure
+
+typedef enum
+{
+  SVGA_DCS_0   = 0x00, /*!< 0 nA (OFF) */
+  SVGA_DCS_0_5 = 0x01, /*!< 0.5 nA     */
+  SVGA_DCS_1   = 0x02  /*!< 1 nA       */
+}SVGA_DCS_TypeDef ; // 
+
 
 //typedef enum
 //{
@@ -76,7 +116,7 @@ typedef enum
 //	__ __;               //
 //	__ __;         //
 //	uint8_t VAL;
-//}SVGA__StructTypeDef; //
+//}SVGA__StructTypeDef; //   structure
 
 //#define TVP_WRITE_ADDRESS 0x1C //SELADR0 = 0
 #define	  SVGA_WRITE_ADDRESS 0x1E //SELADR0 = 1
@@ -84,34 +124,40 @@ typedef enum
 #define	  SVGA_READ_ADDRESS 0x1F  //SELADR0 = 1
 
 #define SVGA_Addr_RevisionInformation            0x00 //
+//Video Related Registers
 #define SVGA_Addr_InputVideoTypeSet              0x01 //
 #define SVGA_Addr_SyncSignalPolaritySet          0x02 //
 #define SVGA_Addr_VerticalBlankLines             0x03 //
 #define SVGA_Addr_HorizontalBlankPixels          0x04 //
 #define SVGA_Addr_AdjustStartActiveVideoPosition 0x05 //
 #define SVGA_Addr_FieldStartLinePosition         0x06 //
-#define SVGA_Addr_DownScaling                    0x07 //*
-#define SVGA_Addr_BrightnessControl              0x08 //*
-#define SVGA_Addr_ContrastControl                0x09 //*
-#define SVGA_Addr_PowerDownModeControl           0x0F //*
-#define SVGA_Addr_DisplayOff                     0x10 //*
-#define SVGA_Addr_DisplayLeftMargin              0x11 //*
-#define SVGA_Addr_DisplayRightMargin             0x12 //*
-#define SVGA_Addr_DisplayTopMargin               0x13 //*
-#define SVGA_Addr_DisplayBottomMargin            0x14 //*
-#define SVGA_Addr_DA_OffsetSetting               0x16 //*
-#define SVGA_Addr_DischargeCurrentSetting        0x17 //*
-#define SVGA_Addr_DischargeEnabledControl        0x18 //*
+#define SVGA_Addr_DownScaling                    0x07 //
+#define SVGA_Addr_BrightnessControl              0x08 //
+#define SVGA_Addr_ContrastControl                0x09 //
+#define SVGA_Addr_PowerDownModeControl           0x0F //
+//Video Display Control Registers
+#define SVGA_Addr_DisplayOff                     0x10 //
+#define SVGA_Addr_DisplayLeftMargin              0x11 //
+#define SVGA_Addr_DisplayRightMargin             0x12 //
+#define SVGA_Addr_DisplayTopMargin               0x13 //
+#define SVGA_Addr_DisplayBottomMargin            0x14 //
+#define SVGA_Addr_DA_OffsetSetting               0x16 //
+#define SVGA_Addr_DischargeCurrentSetting        0x17 //
+#define SVGA_Addr_DischargeEnabledControl        0x18 //
 #define SVGA_Addr_VcomLevelSetting               0x19 //*
+//Temperature Sensor Register
 #define SVGA_Addr_TemperatureSensorReadout       0x1D //*
+//Gamma Look-Up Table Registers
 #define SVGA_Addr_GammaCorrectionLUT0_LSB        0x20 //*
 #define SVGA_Addr_GammaCorrectionLUT0_MSB        0x21 //*
+//Color Offset Control Registers
 #define SVGA_Addr_RedSignalOffset_LSB            0x44 //*
 #define SVGA_Addr_RedSignalOffset_MSB            0x45 //*
 #define SVGA_Addr_GreenSignalOffset_LSB          0x46 //*
 #define SVGA_Addr_GreenSignalOffset_MSB          0x47 //*
 #define SVGA_Addr_BlueSignalOffset_LSB           0x48 //*
 #define SVGA_Addr_BlueSignalOffset_MSB           0x49 //*
+//Test Pattern Generator Control Register
 #define SVGA_Addr_TestPatternModeSelection       0x4A //*
 #define SVGA_Addr_TestPatternLineWidthSetting    0x4B //*
 #define SVGA_Addr_TestPatternLineSpaceSetting    0x4C //*
@@ -119,19 +165,62 @@ typedef enum
 
 void SVGA_Write(uint8_t addr, uint8_t data);
 uint8_t SVGA_Read(uint8_t addr);
-uint8_t SVGA_GetChipRev();
+uint8_t SVGA_GetChipRev(void);
 void SVGA_GetInputVideoTypeSet(SVGA_SCM_StructTypeDef *res);
 void SVGA_SetInputVideoType(SVGA_SM_TypeDef signal_mode, SVGA_SSM_TypeDef sync_signal, SVGA_SCM_TypeDef scan_mode);
 void SVGA_GetSyncSignalPolarity(SVGA_SP_StructTypeDef *res);
 void SVGA_SetSyncSignalPolarity(bool enable_3d, bool refresh_3d, bool v_pol, bool h_pol);
-uint8_t SVGA_GetVerticalBlankLines();
+uint8_t SVGA_GetVerticalBlankLines(void);
 void SVGA_SetVerticalBlankLines(uint8_t v_blank);
-uint8_t SVGA_GetHorizontalBlankPixels();
+uint8_t SVGA_GetHorizontalBlankPixels(void);
 void SVGA_SetHorizontalBlankPixels(uint8_t h_blank);
 SVGA_SVP_TypeDef SVGA_GetStartActiveVideoPosition();
 void SVGA_SetStartActiveVideoPosition(SVGA_SVP_TypeDef v_offset);
 SVGA_SLP_TypeDef SVGA_GetFieldStartLinePosition();
 void SVGA_SetFieldStartLinePosition(SVGA_SLP_TypeDef h_offset);
+
+void SVGA_GetDownScaling(SVGA_DS_StructTypeDef *res);
+void SVGA_SetDownScaling(bool v_scale, SVGA_SD_TypeDef h_scale);
+
+uint8_t SVGA_GetBrightness(void);
+void SVGA_SetBrightness(uint8_t val);
+uint8_t SVGA_GetContrast(void);
+void SVGA_SetContrast(uint8_t val);
+void SVGA_SetDisplayConfig(uint8_t brightness, uint8_t contrast);
+
+void SVGA_GetPowerDown(SVGA_PD_StructTypeDef *res);
+void SVGA_SetPowerDown(bool PDOWN, bool BSGENPD, bool RDACPD, bool RAMPPD, bool VCOMPD, bool TSENPD);
+
+uint8_t SVGA_GetLeftMargin(void);
+void SVGA_SetLeftMargin(uint8_t val);
+uint8_t SVGA_GetRightMargin(void);
+void SVGA_SetRightMargin(uint8_t val);
+uint8_t SVGA_GetTopMargin(void);
+void SVGA_SetTopMargin(uint8_t val);
+uint8_t SVGA_GetBottomMargin(void);
+void SVGA_SetBottomMargin(uint8_t val);
+void SVGA_SetMargin(uint8_t left, uint8_t right, uint8_t top, uint8_t bottom);
+
+uint8_t SVGA_Get_DA_OffsetSetting(void);
+void SVGA_Set_DA_OffsetSetting(uint8_t val);
+
+SVGA_DCS_TypeDef SVGA_GetDischargeCurrent(void);
+void SVGA_SetDischargeCurrent(SVGA_DCS_TypeDef val);
+bool SVGA_GetDischargeEnabled(void);
+void SVGA_SetDischargeEnabled(bool val);
+
+uint8_t SVGA_GetVcomLevel();
+void SVGA_SetVcomLevel(uint8_t val);
+float SVGA_GetTemperatureSensor(void);
+uint16_t SVGA_GetGammaCorrection(uint8_t num);
+void SVGA_SetGammaCorrection(uint8_t num, uint16_t val);
+uint16_t SVGA_GetRedSignalOffset(void);
+void SVGA_SetRedSignalOffset(uint16_t val);
+uint16_t SVGA_GetGreenSignalOffset(void);
+void SVGA_SetGreenSignalOffset(uint16_t val);
+uint16_t SVGA_GetBlueSignalOffset(void);
+void SVGA_SetBlueSignalOffset(uint16_t val);
+void SVGA_SetRGB_SignalOffset(uint16_t red, uint16_t green, uint16_t blue);
 
 
 #endif /*__ svga050_H */
--- Src/svga050.c
+++ Src/svga050.c
@@ -102,4 +102,285 @@ void SVGA_SetFieldStartLinePosition(SVGA_SLP_TypeDef h_offset)
 	SVGA_Write(SVGA_Addr_FieldStartLinePosition, h_offset);
 }
 
+//Down Scaling
+void SVGA_GetDownScaling(SVGA_DS_StructTypeDef *res)
+{
+	res->VAL = SVGA_Read(SVGA_Addr_DownScaling);
+	res->V_Scale = (bool) ((res->VAL >> 2) & 1);
+	res->H_Scale = (SVGA_SD_TypeDef) (res->VAL & 3);
+}
+
+void SVGA_SetDownScaling(bool v_scale, SVGA_SD_TypeDef h_scale)
+{
+	uint8_t tmp;
+	tmp = v_scale * 4 + h_scale;
+	SVGA_Write(SVGA_Addr_DownScaling, tmp);
+}
+
+uint8_t SVGA_GetBrightness()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_BrightnessControl);
+	return res;
+}
+
+void SVGA_SetBrightness(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_BrightnessControl, val);
+}
+
+uint8_t SVGA_GetContrast()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_ContrastControl);
+	return res;
+}
+
+void SVGA_SetContrast(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_ContrastControl, val);
+}
+
+void SVGA_SetImageConfig(uint8_t brightness, uint8_t contrast)
+{
+	SVGA_Write(SVGA_Addr_BrightnessControl, brightness);
+	SVGA_Write(SVGA_Addr_ContrastControl, contrast);
+}
+
+void SVGA_GetPowerDown(SVGA_PD_StructTypeDef *res)
+{
+	res->VAL = SVGA_Read(SVGA_Addr_PowerDownModeControl);
+	res->PDOWN   = (bool) ((res->VAL >> 7) & 1);
+	res->BSGENPD = (bool) ((res->VAL >> 4) & 1);
+	res->RDACPD  = (bool) ((res->VAL >> 3) & 1);
+	res->RAMPPD  = (bool) ((res->VAL >> 2) & 1);
+	res->VCOMPD  = (bool) ((res->VAL >> 1) & 1);
+	res->TSENPD  = (bool)  (res->VAL & 3);
+}
+
+void SVGA_SetPowerDown(bool PDOWN, bool BSGENPD, bool RDACPD, bool RAMPPD, bool VCOMPD, bool TSENPD)
+{
+	uint8_t tmp;
+	tmp = PDOWN * 0x80 + BSGENPD * 0x10 + RDACPD * 8 + RAMPPD * 4 + VCOMPD * 2 + TSENPD;
+	SVGA_Write(SVGA_Addr_PowerDownModeControl, tmp);
+}
+
+void SVGA_GetDisplayOff(SVGA_DO_StructTypeDef *res)
+{
+	res->VAL = SVGA_Read(SVGA_Addr_DisplayOff);
+	res->DispOff = (bool) ((res->VAL >> 2) & 1);
+	res->VSCAN   = (bool) ((res->VAL >> 1) & 1);
+	res->HSCAN   = (bool)  (res->VAL & 3);
+}
+
+void SVGA_SetDisplayOff(bool DispOff, bool VSCAN, bool HSCAN)
+{
+	uint8_t tmp;
+	tmp = DispOff * 4 + VSCAN * 2 + HSCAN;
+	SVGA_Write(SVGA_Addr_DisplayOff, tmp);
+}
+
+uint8_t SVGA_GetLeftMargin()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DisplayLeftMargin);
+	return res;
+}
+
+void SVGA_SetLeftMargin(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_DisplayLeftMargin, val);
+}
+
+uint8_t SVGA_GetRightMargin()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DisplayRightMargin);
+	return res;
+}
+
+void SVGA_SetRightMargin(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_DisplayRightMargin, val);
+}
+
+uint8_t SVGA_GetTopMargin()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DisplayTopMargin);
+	return res;
+}
+
+void SVGA_SetTopMargin(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_DisplayTopMargin, val);
+}
+
+uint8_t SVGA_GetBottomMargin()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DisplayBottomMargin);
+	return res;
+}
+
+void SVGA_SetBottomMargin(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_DisplayBottomMargin, val);
+}
+
+void SVGA_SetMargin(uint8_t left, uint8_t right, uint8_t top, uint8_t bottom)
+{
+	SVGA_Write(SVGA_Addr_DisplayLeftMargin, left);
+	SVGA_Write(SVGA_Addr_DisplayRightMargin, right);
+	SVGA_Write(SVGA_Addr_DisplayTopMargin, top);
+	SVGA_Write(SVGA_Addr_DisplayBottomMargin, bottom);
+}
+
+uint8_t SVGA_Get_DA_OffsetSetting()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DA_OffsetSetting);
+	return res;
+}
+
+void SVGA_Set_DA_OffsetSetting(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_DA_OffsetSetting, val);
+}
+
+SVGA_DCS_TypeDef SVGA_GetDischargeCurrent()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DischargeCurrentSetting);
+	return (SVGA_DCS_TypeDef) res;
+}
+
+void SVGA_SetDischargeCurrent(SVGA_DCS_TypeDef val)
+{
+	SVGA_Write(SVGA_Addr_DischargeCurrentSetting, val);
+}
+
+bool SVGA_GetDischargeEnabled()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_DischargeEnabledControl);
+	return (bool) (res & 1);
+}
+
+void SVGA_SetDischargeEnabled(bool val)
+{
+	SVGA_Write(SVGA_Addr_DischargeEnabledControl, val);
+}
+
+uint8_t SVGA_GetVcomLevel()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_VcomLevelSetting);
+	return res;
+}
+
+void SVGA_SetVcomLevel(uint8_t val)
+{
+	SVGA_Write(SVGA_Addr_VcomLevelSetting, val);
+}
+
+float SVGA_GetTemperatureSensor()
+{
+	uint8_t res;
+	res = SVGA_Read(SVGA_Addr_TemperatureSensorReadout);
+	return 0.47 * res  - 40;
+}
+
+uint16_t SVGA_GetGammaCorrection(uint8_t num)
+{
+	uint16_t res;
+	if (num > 16) return 0;
+	res = SVGA_Read(SVGA_Addr_GammaCorrectionLUT0_MSB + num * 2);
+	res = res << 8;
+	res = res + SVGA_Read(SVGA_Addr_GammaCorrectionLUT0_LSB + num * 2);
+	return res;
+}
+
+void SVGA_SetGammaCorrection(uint8_t num, uint16_t val)
+{
+	uint8_t tmp;
+	if (num > 16) return;
+	tmp = val & 0x0f;
+	SVGA_Write(SVGA_Addr_GammaCorrectionLUT0_LSB + num * 2, tmp);
+	tmp = (val >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_GammaCorrectionLUT0_MSB + num * 2, tmp);
+}
+
+uint16_t SVGA_GetRedSignalOffset()
+{
+	uint16_t res;
+	res = SVGA_Read(SVGA_Addr_RedSignalOffset_MSB);
+	res = res << 8;
+	res = res + SVGA_Read(SVGA_Addr_RedSignalOffset_LSB);
+	return res;
+}
+
+void SVGA_SetRedSignalOffset(uint16_t val)
+{
+	uint8_t tmp;
+	tmp = val & 0x0f;
+	SVGA_Write(SVGA_Addr_RedSignalOffset_LSB, tmp);
+	tmp = (val >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_RedSignalOffset_MSB, tmp);
+}
+
+uint16_t SVGA_GetGreenSignalOffset()
+{
+	uint16_t res;
+	res = SVGA_Read(SVGA_Addr_GreenSignalOffset_MSB);
+	res = res << 8;
+	res = res + SVGA_Read(SVGA_Addr_GreenSignalOffset_LSB);
+	return res;
+}
+
+void SVGA_SetGreenSignalOffset(uint16_t val)
+{
+	uint8_t tmp;
+	tmp = val & 0x0f;
+	SVGA_Write(SVGA_Addr_GreenSignalOffset_LSB, tmp);
+	tmp = (val >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_GreenSignalOffset_MSB, tmp);
+}
+
+uint16_t SVGA_GetBlueSignalOffset()
+{
+	uint16_t res;
+	res = SVGA_Read(SVGA_Addr_BlueSignalOffset_MSB);
+	res = res << 8;
+	res = res + SVGA_Read(SVGA_Addr_BlueSignalOffset_LSB);
+	return res;
+}
+
+void SVGA_SetBlueSignalOffset(uint16_t val)
+{
+	uint8_t tmp;
+	tmp = val & 0x0f;
+	SVGA_Write(SVGA_Addr_BlueSignalOffset_LSB, tmp);
+	tmp = (val >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_BlueSignalOffset_MSB, tmp);
+}
+
+void SVGA_SetRGB_SignalOffset(uint16_t red, uint16_t green, uint16_t blue)
+{
+	uint8_t tmp;
+	tmp = red & 0x0f;
+	SVGA_Write(SVGA_Addr_RedSignalOffset_LSB, tmp);
+	tmp = (red >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_RedSignalOffset_MSB, tmp);
+	tmp = green & 0x0f;
+	SVGA_Write(SVGA_Addr_GreenSignalOffset_LSB, tmp);
+	tmp = (green >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_GreenSignalOffset_MSB, tmp);
+	tmp = blue & 0x0f;
+	SVGA_Write(SVGA_Addr_BlueSignalOffset_LSB, tmp);
+	tmp = (blue >> 8) & 0x01;
+	SVGA_Write(SVGA_Addr_BlueSignalOffset_MSB, tmp);
+}
+
+
 
