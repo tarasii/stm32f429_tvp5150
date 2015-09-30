7d57c0873608df4f3bfd57e919f5ba6d310ae261
tarasii <tarasii@gmail.com>
2015-09-30 13:07:30 +0300

    adxl345 module added

---
+++ Inc/adxl345.h
@@ -0,0 +1,264 @@
+#ifndef __adxl345_H
+#define __adxl345_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "i2c.h"
+
+typedef enum
+{                      /*!< Output Data Rate (Hz)  Bandwidth (Hz)  */
+  ADXL_DR_0_10 = 0x00, /*!<         0,1                0,05        */
+  ADXL_DR_0_20 = 0x01, /*!<         0,2                0,1         */
+  ADXL_DR_0_39 = 0x02, /*!<         0,39               0,2         */
+  ADXL_DR_0_78 = 0x03, /*!<         0,78               0,39        */
+  ADXL_DR_1_56 = 0x04, /*!<         1,56               0,78        */
+  ADXL_DR_3_13 = 0x05, /*!<         3,13               1,56        */
+  ADXL_DR_6_25 = 0x06, /*!<         6,25               3,13        */
+  ADXL_DR_12_5 = 0x07, /*!<         12,5               6,25        */
+  ADXL_DR_25   = 0x08, /*!<         25                 12,5        */
+  ADXL_DR_50   = 0x09, /*!<         50                 25          */
+  ADXL_DR_100  = 0x0A, /*!<         100                50          */
+  ADXL_DR_200  = 0x0B, /*!<         200                100         */
+  ADXL_DR_400  = 0x0C, /*!<         400                200         */
+  ADXL_DR_800  = 0x0D, /*!<         800                400         */
+  ADXL_DR_1600 = 0x0E, /*!<         1600               800         */
+  ADXL_DR_3200 = 0x0F  /*!<         3200               1600        */
+}ADXL_DR_TypeDef ;     // Rate configuration 
+
+typedef enum
+{                   /*!< Frequency (Hz)  */
+  ADXL_WU_8 = 0x00, /*!<      8          */
+  ADXL_WU_4 = 0x01, /*!<      4          */
+  ADXL_WU_2 = 0x02, /*!<      2          */
+  ADXL_WU_1 = 0x03  /*!<      1          */
+}ADXL_WU_TypeDef ;  // Frequency of Readings in Sleep Mode 
+
+typedef enum
+{                     /*!< Range (g)        */
+  ADXL_DFR_2  = 0x00, /*!<      ±2          */
+  ADXL_DFR_4  = 0x01, /*!<      ±4          */
+  ADXL_DFR_8  = 0x02, /*!<      ±8          */
+  ADXL_DFR_16 = 0x03  /*!<      ±16         */
+}ADXL_DFR_TypeDef ;   // Data format range  
+
+typedef enum
+{                         
+  ADXL_FM_BYPASS  = 0x00, /*!< FIFO is bypassed  */
+  ADXL_FM_FIFO    = 0x01, /*!< FIFO collects up to 32 values and then stops collecting data, collecting new data only when FIFO is not full. */
+  ADXL_FM_STREAM  = 0x02, /*!< FIFO holds the last 32 data values. When FIFO is full, the oldest data is overwritten with newer data. */
+  ADXL_FM_TRIGGER = 0x03  /*!< When triggered by the trigger bit, FIFO holds the last data samples before the trigger event and then continues to collect data until full. New data is collected only when FIFO is not full */
+}ADXL_FM_TypeDef ;        // FIFO Modes  
+
+typedef struct
+{
+	ADXL_DR_TypeDef RATE;   //
+	bool LOWPOWER;      //
+	uint8_t VAL;
+}ADXL_DR_StructTypeDef; //   structure
+
+typedef struct
+{
+	bool ACT_ACDC;   //
+	bool ACT_X;      //
+	bool ACT_Y;      //
+	bool ACT_Z;      //
+	bool INACT_ACDC; //
+	bool INACT_X;    //
+	bool INACT_Y;    //
+	bool INACT_Z;    //
+	uint8_t VAL;
+}ADXL_AC_StructTypeDef; // Axis enable control  structure
+
+typedef struct
+{
+	bool DATA_READY;  //
+	bool SINGLE_TAP;  //
+	bool DOUBLE_TAP;  //
+	bool ACTIVITY;    //
+	bool INACTIVITY;  //
+	bool FREE_FALL;   //
+	bool WATTERMARK;  //
+	bool OVERRUN;     //
+	uint8_t VAL;
+}ADXL_IC_StructTypeDef; // Interrupt enable control structure
+
+typedef struct
+{
+	bool SELF_TEST;  //
+	bool SPI;        //
+	bool INT_INVERT; //
+	bool FULL_RES;   //
+	bool JUSTIFY;    //
+	ADXL_DFR_TypeDef RANGE;   //
+	uint8_t VAL;
+}ADXL_DF_StructTypeDef; // Data format control structure
+
+typedef struct
+{
+	bool ACT_X;      //
+	bool ACT_Y;      //
+	bool ACT_Z;      //
+	bool ASLEEP;     //
+	bool TAP_X;      //
+	bool TAP_Y;      //
+	bool TAP_Z;      //
+	uint8_t VAL;
+}ADXL_ATS_StructTypeDef; // Act tap status  structure
+
+typedef struct
+{
+	bool SUPRESS;   //
+	bool TAP_X;     //
+	bool TAP_Y;     //
+	bool TAP_Z;     //
+	uint8_t VAL;
+}ADXL_TA_StructTypeDef; //   structure
+
+typedef struct
+{
+	bool LINK;       //
+	bool AUTO_SLEEP; //
+	bool MEASURE;    //
+	bool SLEEP;      //
+	ADXL_WU_TypeDef WAKEUP;  //
+	uint8_t VAL;
+}ADXL_PS_StructTypeDef; //   structure
+
+typedef struct
+{
+	bool FIFO_TRIG;   //
+	ADXL_FM_TypeDef FIFO_MODE;  //
+	uint8_t SAMPLES;  //
+	uint8_t VAL;
+}ADXL_FS_StructTypeDef; //   structure
+
+typedef struct
+{
+	bool FIFO_TRIG;   //
+	uint8_t ENTRIES;  //
+	uint8_t VAL;
+}ADXL_FC_StructTypeDef; //   structure
+
+typedef struct
+{
+	uint16_t X;    //
+	uint16_t Y;    //
+	uint16_t Z;    //
+}ADXL_XYZ_StructTypeDef; //   structure
+
+
+//typedef enum
+//{
+//  ITG_         = 0x00, /*!<              */
+//  ITG_         = 0x01, /*!<              */
+//  ITG_         = 0x02, /*!<              */
+//  ITG_         = 0x03  /*!<              */
+//}ITG__TypeDef ; // 
+//
+//typedef struct
+//{
+//	__ __;               //
+//	__ __;               //
+//	__ __;         //
+//	uint8_t VAL;
+//}ITG__StructTypeDef; //   structure
+
+
+#define		ADXL_WRITE_ADDRESS 0x3A //ALT ADDRESS = 1
+//#define	ADXL_WRITE_ADDRESS 0xA6 //ALT ADDRESS = 0
+#define		ADXL_READ_ADDRESS 0x3B  //ALT ADDRESS = 1
+//#define	ADXL_READ_ADDRESS 0xA7  //ALT ADDRESS = 0
+
+#define ADXL_Addr_DEVID          0x00 // r
+#define ADXL_Addr_TAP_THRESH     0x1D // rw
+#define ADXL_Addr_OFSX           0x1E //
+#define ADXL_Addr_OFSY           0x1F //
+#define ADXL_Addr_OFSZ           0x20 //
+#define ADXL_Addr_TAP_DUR        0x21 //
+#define ADXL_Addr_TAP_LATENT     0x22 //
+#define ADXL_Addr_TAP_WINDOW     0x23 //
+#define ADXL_Addr_ACT_THRESH     0x24 //
+#define ADXL_Addr_INACT_THRESH   0x25 //
+#define ADXL_Addr_INACT_TIME     0x26 //
+#define ADXL_Addr_ACT_INACT_CTL  0x27 //
+#define ADXL_Addr_FF_THRESH      0x28 //
+#define ADXL_Addr_FF_TIME        0x29 // 
+#define ADXL_Addr_TAP_AXES       0x2A // rw
+#define ADXL_Addr_ACT_TAP_STATUS 0x2B // r
+#define ADXL_Addr_BW_RATE        0x2C // rw
+#define ADXL_Addr_POWER_CTL      0x2D //
+#define ADXL_Addr_INT_ENABLE     0x2E //
+#define ADXL_Addr_INT_MAP        0x2F // rw
+#define ADXL_Addr_INT_SOURCE     0x30 // r
+#define ADXL_Addr_DATA_FORMAT    0x31 // rw
+#define ADXL_Addr_DATAX0         0x32 // r
+#define ADXL_Addr_DATAX1         0x33 //
+#define ADXL_Addr_DATAY0         0x34 //
+#define ADXL_Addr_DATAY1         0x35 //
+#define ADXL_Addr_DATAZ0         0x36 //
+#define ADXL_Addr_DATAZ1         0x37 //
+#define ADXL_Addr_FIFO_CTL       0x38 //* rw
+#define ADXL_Addr_FIFO_STATUS    0x39 //* r
+//#define ADXL_Addr_             0x00 //*
+
+
+void ADXL_Init(void);
+void ADXL_WriteByte(uint8_t addr, uint8_t data);
+uint8_t ADXL_ReadByte(uint8_t addr);
+uint8_t ADXL_GetDeviceId(void);
+uint8_t ADXL_GetTapThreshold(void);
+void ADXL_SetTapThreshold(uint8_t res);
+uint8_t ADXL_GetX_Offset(void);
+void ADXL_SetX_Offset(uint8_t res);
+uint8_t ADXL_GetY_Offset(void);
+void ADXL_SetY_Offset(uint8_t res);
+uint8_t ADXL_GetZ_Offset(void);
+void ADXL_SetZ_Offset(uint8_t res);
+void ADXL_SetXYZ_Offset(uint8_t x, uint8_t y, uint8_t z);
+uint8_t ADXL_GetTapDuration(void);
+void ADXL_SetTapDuration(uint8_t res);
+uint8_t ADXL_GetTapLatency(void);
+void ADXL_SetTapLatency(uint8_t res);
+uint8_t ADXL_GetTapWindow(void);
+void ADXL_SetTapWindow(uint8_t res);
+uint8_t ADXL_GetActivityThreshold(void);
+void ADXL_SetActivityThreshold(uint8_t res);
+uint8_t ADXL_GetInactivityThreshold(void);
+void ADXL_SetInactivityThreshold(uint8_t res);
+uint8_t ADXL_GetInactivityTime(void);
+void ADXL_SetInactivityTime(uint8_t res);
+void ADXL_GetAxisControl(ADXL_AC_StructTypeDef *res);
+void ADXL_SetAxisControl(bool act_acdc, bool act_x, bool act_y, bool act_z, bool inact_acdc, bool inact_x, bool inact_y, bool inact_z);
+uint8_t ADXL_GetFreeFallThreshold(void);
+void ADXL_SetFreeFallThreshold(uint8_t res);
+uint8_t ADXL_GetFreeFallTime(void);
+void ADXL_SetFreeFallTime(uint8_t res);
+void ADXL_GetTapAxis(ADXL_TA_StructTypeDef *res);
+void ADXL_SetTapAxis(bool supress, bool tap_x, bool tap_y, bool tap_z);
+void ADXL_GetActTapStatus(ADXL_ATS_StructTypeDef *res);
+void ADXL_GetDataRatePowerMode(ADXL_DR_StructTypeDef *res);
+void ADXL_SetDataRatePowerMode(bool low_power, ADXL_DR_TypeDef rate);
+void ADXL_GetPowerSaving(ADXL_PS_StructTypeDef *res);
+void ADXL_SetPowerSaving(bool link, bool auto_sleep, bool measure, bool sleep, ADXL_WU_TypeDef wakeup);
+void ADXL_GetIntConfig(ADXL_IC_StructTypeDef *res);
+void ADXL_SetIntConfig(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun);
+void ADXL_GetIntMap(ADXL_IC_StructTypeDef *res);
+void ADXL_SetIntMap(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun);
+void ADXL_GetIntSource(ADXL_IC_StructTypeDef *res);
+void ADXL_SetIntSource(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun);
+void ADXL_GetDataFormat(ADXL_DF_StructTypeDef *res);
+void ADXL_SetDataFormat(bool self_test, bool spi, bool int_invert, bool full_res, bool justify, ADXL_DFR_TypeDef range);
+void ADXL_SelfTest(void);
+uint16_t ADXL_GetX(void);
+uint16_t ADXL_GetY(void);
+uint16_t ADXL_GetZ(void);
+void ADXL_GetXYZ(ADXL_XYZ_StructTypeDef *res);
+void ADXL_GetFIFO_Status(ADXL_FC_StructTypeDef *res);
+void ADXL_GetFIFO_Control(ADXL_FS_StructTypeDef *res);
+void ADXL_SetFIFO_Control(ADXL_FM_TypeDef fifo_mode, bool trigger, uint8_t samples);
+	
+
+
+//Taras Ivaniv
+
+#endif /*__ adxl345_H */
---
+++ Src/adxl345.c
@@ -0,0 +1,410 @@
+#include "adxl345.h"
+
+void ADXL_Init()
+{
+	return;
+}
+
+
+void ADXL_WriteByte(uint8_t addr, uint8_t data)
+{
+	uint8_t i2cbuf[2];
+	i2cbuf[0] = addr;
+	i2cbuf[1] = data;
+	I2C1_WriteBuffer(ADXL_WRITE_ADDRESS, i2cbuf, 2);
+}
+
+uint8_t ADXL_ReadByte(uint8_t addr)
+{
+	uint8_t i2cbuf[1];
+  I2C1_WriteBuffer(ADXL_READ_ADDRESS, &addr, 1);
+	I2C1_ReadBuffer(ADXL_READ_ADDRESS, addr, i2cbuf, 1);
+	return i2cbuf[0];
+}
+
+
+uint8_t ADXL_GetDeviceId()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_DEVID);
+	return res << 1;
+}
+
+uint8_t ADXL_GetTapThreshold()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_TAP_THRESH);
+	return res;
+}
+
+void ADXL_SetTapThreshold(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_TAP_THRESH, res);
+}
+
+uint8_t ADXL_GetX_Offset()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_OFSX);
+	return res;
+}
+
+void ADXL_SetX_Offset(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_OFSX, res);
+}
+
+uint8_t ADXL_GetY_Offset()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_OFSY);
+	return res;
+}
+
+void ADXL_SetY_Offset(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_OFSY, res);
+}
+
+uint8_t ADXL_GetZ_Offset()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_OFSZ);
+	return res;
+}
+
+void ADXL_SetZ_Offset(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_OFSZ, res);
+}
+
+void ADXL_SetXYZ_Offset(uint8_t x, uint8_t y, uint8_t z)
+{
+	ADXL_WriteByte(ADXL_Addr_OFSX, x);
+	ADXL_WriteByte(ADXL_Addr_OFSY, y);
+	ADXL_WriteByte(ADXL_Addr_OFSZ, z);
+}
+
+uint8_t ADXL_GetTapDuration()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_TAP_DUR);
+	return res;
+}
+
+void ADXL_SetTapDuration(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_TAP_DUR, res);
+}
+
+uint8_t ADXL_GetTapLatency()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_TAP_LATENT);
+	return res;
+}
+
+void ADXL_SetTapLatency(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_TAP_LATENT, res);
+}
+
+uint8_t ADXL_GetTapWindow()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_TAP_WINDOW);
+	return res;
+}
+
+void ADXL_SetTapWindow(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_TAP_WINDOW, res);
+}
+
+uint8_t ADXL_GetActivityThreshold()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_ACT_THRESH);
+	return res;
+}
+
+void ADXL_SetActivityThreshold(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_ACT_THRESH, res);
+}
+
+uint8_t ADXL_GetInactivityThreshold()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_INACT_THRESH);
+	return res;
+}
+
+void ADXL_SetInactivityThreshold(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_INACT_THRESH, res);
+}
+
+uint8_t ADXL_GetInactivityTime()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_INACT_TIME);
+	return res;
+}
+
+void ADXL_SetInactivityTime(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_INACT_TIME, res);
+}
+
+
+void ADXL_GetAxisControl(ADXL_AC_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_ACT_INACT_CTL);
+	res->ACT_ACDC   = (bool) ((res->VAL >> 7) & 1);
+	res->ACT_X      = (bool) ((res->VAL >> 6) & 1);
+	res->ACT_Y      = (bool) ((res->VAL >> 5) & 1);
+	res->ACT_Z      = (bool) ((res->VAL >> 4) & 1);
+	res->INACT_ACDC = (bool) ((res->VAL >> 3) & 1);
+	res->INACT_X    = (bool) ((res->VAL >> 2) & 1);
+	res->INACT_Y    = (bool) ((res->VAL >> 1) & 1);
+	res->INACT_Z    = (bool) ( res->VAL       & 1);	
+}
+
+void ADXL_SetAxisControl(bool act_acdc, bool act_x, bool act_y, bool act_z, bool inact_acdc, bool inact_x, bool inact_y, bool inact_z)
+{
+	uint8_t tmp;
+	tmp = act_acdc * 0x80 + act_x * 0x40 + act_y * 0x20 + act_z * 0x10 + inact_acdc * 8 + inact_x * 4 + inact_y * 2 + inact_z;
+	ADXL_WriteByte(ADXL_Addr_ACT_INACT_CTL, tmp);
+}
+
+uint8_t ADXL_GetFreeFallThreshold()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_FF_THRESH);
+	return res;
+}
+
+void ADXL_SetFreeFallThreshold(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_FF_THRESH, res);
+}
+
+uint8_t ADXL_GetFreeFallTime()
+{
+	uint8_t res;
+	res = ADXL_ReadByte(ADXL_Addr_FF_TIME);
+	return res;
+}
+
+void ADXL_SetFreeFallTime(uint8_t res)
+{
+	ADXL_WriteByte(ADXL_Addr_FF_TIME, res);
+}
+
+void ADXL_GetTapAxis(ADXL_TA_StructTypeDef *res)
+{
+	res->VAL      = ADXL_ReadByte(ADXL_Addr_TAP_AXES);
+	res->SUPRESS  = (bool) ((res->VAL >> 3) & 1);
+	res->TAP_X    = (bool) ((res->VAL >> 2) & 1);
+	res->TAP_Y    = (bool) ((res->VAL >> 1) & 1);
+	res->TAP_Z    = (bool) ( res->VAL       & 1);	
+}
+
+void ADXL_SetTapAxis(bool supress, bool tap_x, bool tap_y, bool tap_z)
+{
+	uint8_t tmp;
+	tmp = supress * 8 + tap_x * 4 + tap_y * 2 + tap_z;
+	ADXL_WriteByte(ADXL_Addr_TAP_AXES, tmp);
+}
+
+void ADXL_GetActTapStatus(ADXL_ATS_StructTypeDef *res)
+{
+	res->VAL      = ADXL_ReadByte(ADXL_Addr_ACT_TAP_STATUS);
+	res->ACT_X    = (bool) ((res->VAL >> 6) & 1);
+	res->ACT_Y    = (bool) ((res->VAL >> 5) & 1);
+	res->ACT_Z    = (bool) ((res->VAL >> 4) & 1);
+	res->ASLEEP   = (bool) ((res->VAL >> 3) & 1);
+	res->TAP_X    = (bool) ((res->VAL >> 2) & 1);
+	res->TAP_Y    = (bool) ((res->VAL >> 1) & 1);
+	res->TAP_Z    = (bool) ( res->VAL       & 1);	
+}
+
+
+void ADXL_GetDataRatePowerMode(ADXL_DR_StructTypeDef *res)
+{
+	res->VAL      = ADXL_ReadByte(ADXL_Addr_BW_RATE);
+	res->LOWPOWER = (bool)            ((res->VAL >> 4) & 1  );
+	res->RATE     = (ADXL_DR_TypeDef) ( res->VAL       & 0xF);	
+}
+
+void ADXL_SetDataRatePowerMode(bool low_power, ADXL_DR_TypeDef rate)
+{
+	uint8_t tmp;
+	tmp = low_power * 0x10 + (rate & 0x0F);
+	ADXL_WriteByte(ADXL_Addr_BW_RATE, tmp);
+}
+
+void ADXL_GetPowerSaving(ADXL_PS_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_POWER_CTL);
+	res->LINK       = (bool) ((res->VAL >> 5) & 1);
+	res->AUTO_SLEEP = (bool) ((res->VAL >> 4) & 1);
+	res->MEASURE    = (bool) ((res->VAL >> 3) & 1);
+	res->SLEEP      = (bool) ((res->VAL >> 2) & 1);
+	res->WAKEUP     = (ADXL_WU_TypeDef) ( res->VAL       & 3);	
+}
+
+void ADXL_SetPowerSaving(bool link, bool auto_sleep, bool measure, bool sleep, ADXL_WU_TypeDef wakeup)
+{
+	uint8_t tmp;
+	tmp = link * 0x20 + auto_sleep * 0x10 + measure * 8 + sleep * 4 + (wakeup & 3);
+	ADXL_WriteByte(ADXL_Addr_POWER_CTL, tmp);
+}
+
+void ADXL_GetIntConfig(ADXL_IC_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_INT_ENABLE);
+	res->DATA_READY = (bool) ((res->VAL >> 7) & 1);
+	res->SINGLE_TAP = (bool) ((res->VAL >> 6) & 1);
+	res->DOUBLE_TAP = (bool) ((res->VAL >> 5) & 1);
+	res->ACTIVITY   = (bool) ((res->VAL >> 4) & 1);
+	res->INACTIVITY = (bool) ((res->VAL >> 3) & 1);
+	res->FREE_FALL  = (bool) ((res->VAL >> 2) & 1);
+	res->WATTERMARK = (bool) ((res->VAL >> 1) & 1);
+	res->OVERRUN    = (bool) ( res->VAL       & 1);	
+}
+
+void ADXL_SetIntConfig(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun)
+{
+	uint8_t tmp;
+	tmp = data_ready * 0x80 + single_tap * 0x40 + double_tap * 0x20 + activity * 0x10 + inactivity * 8 + free_fall * 4 + watermark * 2 + overrun;
+	ADXL_WriteByte(ADXL_Addr_INT_ENABLE, tmp);
+}
+
+void ADXL_GetIntMap(ADXL_IC_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_INT_MAP);
+	res->DATA_READY = (bool) ((res->VAL >> 7) & 1);
+	res->SINGLE_TAP = (bool) ((res->VAL >> 6) & 1);
+	res->DOUBLE_TAP = (bool) ((res->VAL >> 5) & 1);
+	res->ACTIVITY   = (bool) ((res->VAL >> 4) & 1);
+	res->INACTIVITY = (bool) ((res->VAL >> 3) & 1);
+	res->FREE_FALL  = (bool) ((res->VAL >> 2) & 1);
+	res->WATTERMARK = (bool) ((res->VAL >> 1) & 1);
+	res->OVERRUN    = (bool) ( res->VAL       & 1);	
+}
+
+void ADXL_SetIntMap(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun)
+{
+	uint8_t tmp;
+	tmp = data_ready * 0x80 + single_tap * 0x40 + double_tap * 0x20 + activity * 0x10 + inactivity * 8 + free_fall * 4 + watermark * 2 + overrun;
+	ADXL_WriteByte(ADXL_Addr_INT_MAP, tmp);
+}
+
+void ADXL_GetIntSource(ADXL_IC_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_INT_SOURCE);
+	res->DATA_READY = (bool) ((res->VAL >> 7) & 1);
+	res->SINGLE_TAP = (bool) ((res->VAL >> 6) & 1);
+	res->DOUBLE_TAP = (bool) ((res->VAL >> 5) & 1);
+	res->ACTIVITY   = (bool) ((res->VAL >> 4) & 1);
+	res->INACTIVITY = (bool) ((res->VAL >> 3) & 1);
+	res->FREE_FALL  = (bool) ((res->VAL >> 2) & 1);
+	res->WATTERMARK = (bool) ((res->VAL >> 1) & 1);
+	res->OVERRUN    = (bool) ( res->VAL       & 1);	
+}
+
+void ADXL_SetIntSource(bool data_ready, bool single_tap, bool double_tap, bool activity, bool inactivity, bool free_fall, bool watermark, bool overrun)
+{
+	uint8_t tmp;
+	tmp = data_ready * 0x80 + single_tap * 0x40 + double_tap * 0x20 + activity * 0x10 + inactivity * 8 + free_fall * 4 + watermark * 2 + overrun;
+	ADXL_WriteByte(ADXL_Addr_INT_SOURCE, tmp);
+}
+
+void ADXL_GetDataFormat(ADXL_DF_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_DATA_FORMAT);
+	res->SELF_TEST  = (bool) ((res->VAL >> 7) & 1);
+	res->SPI        = (bool) ((res->VAL >> 6) & 1);
+	res->INT_INVERT = (bool) ((res->VAL >> 5) & 1);
+	res->FULL_RES   = (bool) ((res->VAL >> 3) & 1);
+	res->JUSTIFY    = (bool) ((res->VAL >> 2) & 1);
+	res->RANGE      = (ADXL_DFR_TypeDef) ( res->VAL       & 3);	
+}
+
+void ADXL_SetDataFormat(bool self_test, bool spi, bool int_invert, bool full_res, bool justify, ADXL_DFR_TypeDef range)
+{
+	uint8_t tmp;
+	tmp = self_test * 0x80 + spi * 0x40 + int_invert * 0x10 + full_res * 8 + justify * 4 + (range & 3);
+	ADXL_WriteByte(ADXL_Addr_DATA_FORMAT, tmp);
+}
+
+void ADXL_SelfTest()
+{
+	ADXL_DF_StructTypeDef tmp;
+	ADXL_GetDataFormat(&tmp);
+	ADXL_SetDataFormat(true, tmp.SPI, tmp.INT_INVERT, tmp.FULL_RES, tmp.JUSTIFY, tmp.RANGE);
+}
+
+uint16_t ADXL_GetX()
+{
+	uint16_t res = 0;
+	res = ADXL_ReadByte(ADXL_Addr_DATAX0);
+	res = res << 8;
+	res = res + ADXL_ReadByte(ADXL_Addr_DATAX1);
+	return res;
+}
+
+uint16_t ADXL_GetY()
+{
+	uint16_t res = 0;
+	res = ADXL_ReadByte(ADXL_Addr_DATAY0);
+	res = res << 8;
+	res = res + ADXL_ReadByte(ADXL_Addr_DATAY1);
+	return res;
+}
+
+uint16_t ADXL_GetZ()
+{
+	uint16_t res = 0;
+	res = ADXL_ReadByte(ADXL_Addr_DATAZ0);
+	res = res << 8;
+	res = res + ADXL_ReadByte(ADXL_Addr_DATAZ1);
+	return res;
+}
+
+void ADXL_GetXYZ(ADXL_XYZ_StructTypeDef *res)
+{
+	res->X = ADXL_GetX();
+	res->Y = ADXL_GetY();
+	res->Z = ADXL_GetZ();
+}
+
+
+void ADXL_GetFIFO_Status(ADXL_FC_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_FIFO_STATUS);
+	res->FIFO_TRIG  = (bool)    ((res->VAL >> 7) & 1   );
+	res->ENTRIES    = (uint8_t) ( res->VAL       & 0x3F);	
+}
+
+void ADXL_GetFIFO_Control(ADXL_FS_StructTypeDef *res)
+{
+	res->VAL        = ADXL_ReadByte(ADXL_Addr_FIFO_CTL);
+	res->FIFO_MODE  = (ADXL_FM_TypeDef) ((res->VAL >> 6) & 3   );
+	res->FIFO_TRIG  = (bool)            ((res->VAL >> 5) & 1   );
+	res->SAMPLES    = (uint8_t)          ( res->VAL      & 0x1F);	
+}
+
+void ADXL_SetFIFO_Control(ADXL_FM_TypeDef fifo_mode, bool trigger, uint8_t samples)
+{
+	uint8_t tmp;
+	tmp = fifo_mode * 0x40 + trigger * 0x20 + (samples & 0x1F);
+	ADXL_WriteByte(ADXL_Addr_FIFO_CTL, tmp);
+}
+
+
+
+
+
